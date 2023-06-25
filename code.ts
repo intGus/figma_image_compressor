efe5938d37ad70cefb052ff6eda9336309d81aee
intgus <46197948+intGus@users.noreply.github.com>
2023-06-24 22:14:53 -0400

    fix a memory leak on web and bug preventing exporting nodes without export settings

--- code.ts
+++ code.ts
@@ -2,14 +2,15 @@ figma.showUI(__html__)
 
 figma.ui.resize(500, 600)
 
-main()
+main().then(res => figma.closePlugin(res))
 
 async function main() {
   const blobs = [];
   const { selection } = figma.currentPage;
 
   for (let node of selection) {
-    const scale = node.exportSettings[0].constraint.value || 1;
+    console.log(node)
+    const scale = node.exportSettings[0] ? node.exportSettings[0].constraint.value : 1; // Check export settings or default to 1
     const blobPromise = exportPostNodeAsJPG(node, scale);
     blobs.push([node.name, scale, blobPromise]);
   }
@@ -18,33 +19,34 @@ async function main() {
     blobs.map(async ([nodeName, scale, blobPromise]) => [nodeName, scale, await blobPromise])
   );
   figma.ui.postMessage(resolvedBlobs);
+  
+  return new Promise(res => {
+    figma.ui.onmessage = () => res()
+  })
+}
 
-  async function exportFrameAsJPG(frameNode, scale) {
-    const options = {
-      format: 'JPG',
-      constraint: {
-        type: 'SCALE',
-        value: scale // Get the value from Figma's export setting
-      }
-    };
-
-    const bytes = await frameNode.exportAsync(options);
-    return new Uint8Array(bytes);
-  }
-
-  async function exportPostNodeAsJPG(node, scale) {
-    const postFrame = node;
-
-    if (!postFrame) {
-      return;
+async function exportFrameAsJPG(frameNode, scale) {
+  const options = {
+    format: 'JPG',
+    constraint: {
+      type: 'SCALE',
+      value: scale // Get the value from Figma's export setting
     }
+  };
+  const bytes = await frameNode.exportAsync(options);
+  return new Uint8Array(bytes);
+}
 
-    try {
-      const imageBytes = await exportFrameAsJPG(postFrame, scale);
-      return imageBytes;
-    } catch (error) {
-      console.error('Error exporting frame as JPG:', error);
-      throw error; // Rethrow the error to propagate it
-    }
+async function exportPostNodeAsJPG(node, scale) {
+  const postFrame = node;
+  if (!postFrame) {
+    return;
   }
-};
+  try {
+    const imageBytes = await exportFrameAsJPG(postFrame, scale);
+    return imageBytes;
+  } catch (error) {
+    console.error('Error exporting frame as JPG:', error);
+    throw error; // Rethrow the error to propagate it
+  }
+}
